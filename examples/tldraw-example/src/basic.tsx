d0ba3d68e3ea735a2912d2dc08c04e4aef1265ae
Neo <nsheikh@freelancer.com>
2024-07-31 15:33:51 +1000

    Improvements to multiplayer

--- examples/tldraw-example/src/basic.tsx
+++ examples/tldraw-example/src/basic.tsx
@@ -3,6 +3,7 @@ import { TDAsset, TDAssets, TDShape, TDUser, TDUserStatus, Tldraw, TldrawApp } f
 import { useThrottle } from '@uidotdev/usehooks'
 import * as React from 'react'
 import * as Y from 'yjs'
+
 type AwarenessInfo = TDUser & {
   color: string
 }
@@ -14,35 +15,31 @@ interface AwarenessItem {
   }[]
 }
 
-
-
 export default function Basic() {
   const provider = React.useMemo(
     () =>
       new HocuspocusProvider({
         url: 'ws://localhost:1235',
-        name: '124',
+        name: 'playground:1',
         token: 'super-secret-token',
+        connect: false,
       }),
     []
   )
+  const yDoc = React.useRef<Y.Doc>()
 
-  const [val, setVal] = React.useState({
-    shapes: new Map<string, TDShape>(),
-    assets: new Map<string, TDAsset>(),
-    
-  });
-
-  const yDoc = provider.document
   const application = React.useRef<TldrawApp>()
   const previousStateObjectIds = React.useRef(new Set<string>())
   const handleShapeChanges = (value: Uint8Array) => {
+    if (!yDoc.current) return
+
     const shapeMap = new Map<string, TDShape>()
-    for (let [_, v] of yDoc.getMap<TDShape>('shapes').entries()) {
+    for (let [_, v] of yDoc.current.getMap<TDShape>('shapes').entries()) {
       shapeMap.set(v.id, v)
     }
     const seen = new Set<string>()
     const toDeleteIds: string[] = []
+
     application.current?.shapes.forEach((value) => {
       seen.add(value.id)
       const updatedShape = shapeMap.get(value.id)
@@ -54,22 +51,25 @@ export default function Basic() {
         toDeleteIds.push(value.id)
       }
     })
-    // Handle the deletions
+
     application.current?.delete(toDeleteIds)
     // loop through to find out if we need to create stuff
     for (let [i, value] of shapeMap.entries()) {
       if (!seen.has(value.id)) {
         const newShape = shapeMap.get(i)
+
         if (newShape) {
-          application.current?.createShapes(newShape)
+          console.log('creating shape', newShape)
+          application.current?.create([newShape])
         }
       }
     }
   }
 
   const handleAssetChanges = (value: Uint8Array) => {
+    if (!yDoc.current) return
     const assetMap = new Map<string, TDAsset>()
-    for (let [_, v] of yDoc.getMap<TDAsset>('assets').entries()) {
+    for (let [_, v] of yDoc.current.getMap<TDAsset>('assets').entries()) {
       assetMap.set(v.id, v)
     }
     const seen = new Set<string>()
@@ -95,90 +95,86 @@ export default function Basic() {
     application.current?.patchAssets(assetsToPatch)
   }
   const [update, setUpdate] = useTlDrawUpdates()
+
   React.useEffect(() => {
-    if(update){
+    if (update) {
       handleShapeChanges(update)
       handleAssetChanges(update)
     }
-  },[update])
+  }, [update])
+
   const [awarenessUpdate, setAwarenessUpdate] = useTlDrawAwarenessUpdates()
-  const onStateUpdate = React.useCallback( ({ states }: AwarenessItem) => {
-    const awareness = provider.awareness
-    if (!application.current || !application.current.room || !awareness) return
-    const newUsersToUpdate: TDUser[] = []
-    for (let state of states) {
-      if (awareness.clientID !== state.clientId && state.tdUser !== undefined) {
-        newUsersToUpdate.push({
-          id: state.clientId.toString(),
-          point: state.tdUser.point,
-          status: TDUserStatus.Connected,
-          activeShapes: [],
-          color: state.tdUser.color,
-          session: state.tdUser.session,
-          selectedIds: state.tdUser.selectedIds,
-        })
+  const onStateUpdate = React.useCallback(
+    ({ states }: AwarenessItem) => {
+      const awareness = provider.awareness
+      if (!application.current || !application.current.room || !awareness) return
+
+      const newUsersToUpdate: TDUser[] = []
+      for (const state of states) {
+        if (awareness.clientID !== state.clientId && state.tdUser !== undefined) {
+          const user: TDUser = {
+            id: state.clientId.toString(),
+            point: state.tdUser.point,
+            status: TDUserStatus.Connected,
+            activeShapes: [],
+            color: state.tdUser.color,
+            session: state.tdUser.session,
+            selectedIds: state.tdUser.selectedIds,
+          }
+          newUsersToUpdate.push(user)
+        }
       }
-    }
-    application.current.updateUsers(newUsersToUpdate, true)
-  },[application])
+
+      // remove users that are no longer in the room - more than 2 seconds have passed
+      const statesToRemove = states.filter(
+        (state) => state.tdUser?.metadata && state.tdUser.metadata?.timestamp - Date.now() > 2000
+      )
+
+      statesToRemove.forEach((state) => {
+        application.current?.removeUser(state.tdUser.id)
+      })
+
+      application.current.updateUsers(newUsersToUpdate, true)
+    },
+    [application]
+  )
 
   React.useEffect(() => {
-    if(awarenessUpdate){
+    if (awarenessUpdate) {
       onStateUpdate(awarenessUpdate)
     }
   })
-  // Deal with awareness
-
-
-  const ownAwarenessUpdateSubject = (user: TDUser) => {
-    const awareness = provider.awareness
-    if (awareness) {
-      awareness.setLocalStateField('tdUser', user)
-    }
-  }
-
 
   React.useEffect(() => {
-    if (!yDoc || !provider) {
-      console.log(
-        'yDoc or provider not available. This should not happen. Please check the code.', yDoc, provider
-      )
-      return
-    }
-    yDoc.on('update', setUpdate)
+    provider.connect()
+    yDoc.current = provider.document
+    yDoc.current.on('update', setUpdate)
     provider.on('awarenessUpdate', (states: AwarenessItem) => {
       setAwarenessUpdate(states)
     })
-    console.log('subscribing')
-    return () => {
-      console.log('unsubscribing')
-      yDoc._observers.clear()
-      provider.removeAllListeners()
-    }
-
-  }, [yDoc, provider, onStateUpdate, setUpdate])
+  }, [])
 
   return (
     <Editor
-      application={application}
       yDoc={yDoc}
+      provider={provider}
+      application={application}
       previousStateObjectIds={previousStateObjectIds}
-      ownAwarenessUpdateSubject={ownAwarenessUpdateSubject}
+    
     />
   )
 }
 
-
-const Editor = React.memo(function Editor ({
-  application,
+const Editor = React.memo(function Editor({
   yDoc,
+  provider,
+  application,
   previousStateObjectIds,
-  ownAwarenessUpdateSubject,
-}:{
+}: {
+  yDoc: React.MutableRefObject<Y.Doc | undefined>
+  provider: HocuspocusProvider
   application: React.MutableRefObject<TldrawApp | undefined>
-  yDoc: Y.Doc
   previousStateObjectIds: React.MutableRefObject<Set<string>>
-  ownAwarenessUpdateSubject: (user: TDUser) => void
 }) {
   const r = Math.floor(Math.random() * 128 + 100)
   const g = Math.floor(Math.random() * 128 + 100)
@@ -187,11 +183,26 @@ const Editor = React.memo(function Editor ({
     .toString(16)
     .padStart(2, '0')}`
 
+  const ownAwarenessUpdateSubject = (user: TDUser) => {
+    yDoc
+    const awareness = provider.awareness
 
+    const ownAwareness = awareness === application.current?.currentUser?.id
 
+    if (awareness && ownAwareness) {
+      awareness.setLocalStateField('tdUser', user)
+    }
+  }
+
+  const x = useTraceUpdate({ provider, application, previousStateObjectIds })
+  console.log('rendering Editor', x)
   return (
     <div className="tldraw">
       <Tldraw
+      showMenu={false}
+      
+      onUndo={undefined}
+      onRedo={undefined}
         onAssetCreate={async (app, file, id) => {
           const toBase64 = (file: File) =>
             new Promise<string>((resolve, reject) => {
@@ -207,84 +218,104 @@ const Editor = React.memo(function Editor ({
         }}
         onMount={(a: TldrawApp) => {
           application.current = a
-          application.current.updateUsers([]);
+          application.current.updateUsers([])
           application.current.loadRoom('124')
         }}
         onPersist={(app: TldrawApp) => {
           // if app is loading don't persist anything.
-          if (!application.current || application.current.isLoading) return
+          if (!application.current || application.current.isLoading || !yDoc.current) {
+            console.log('Nothing to update')
+            return
+          }
 
           /********* SHAPES *********/
-          const newStateObjectIds = new Set<string>()
-          yDoc.transact(() => {
-            const yDocShapes = yDoc.getMap('shapes')
-            Object.entries(app.shapes).forEach(([id, shape]) => {
-              newStateObjectIds.add(id)
-              previousStateObjectIds.current.delete(id)
-              if (!yDocShapes.get(id)) {
-                // create a new object
-                yDocShapes.set(id, shape)
-              } else {
-                const shapeToUpdate = yDocShapes.get(id)
-                if (JSON.stringify(shapeToUpdate) !== JSON.stringify(shape)) {
-                  // update existing object
-                  yDocShapes.set(id, shape)
+          try {
+            yDoc.current.transact(() => {
+              const yDocShapes = yDoc.current!.getMap<TDShape>('shapes')
+              const currentShapeIds = new Set(app.shapes.map((shape) => shape.id))
+
+              // Delete shapes that are in yDoc but not in app.shapes
+              for (const [_, docShape] of yDocShapes) {
+                if (!currentShapeIds.has(docShape.id)) {
+                  console.log('Deleting shape from yDoc:', docShape.id)
+                  yDocShapes.delete(docShape.id)
+                }
+              }
+
+              // Update or add shapes
+              Object.entries(app.shapes).forEach(([_, shape]) => {
+                const existingShape = yDocShapes.get(shape.id)
+                if (!existingShape || JSON.stringify(existingShape) !== JSON.stringify(shape)) {
+                  console.log('Updating/Adding shape in yDoc:', shape.id)
+                  yDocShapes.set(shape.id, shape)
+                }
+              })
+
+              console.log('Shapes persisted')
+            })
+
+            /********* ASSETS *********/
+            yDoc.current.transact(() => {
+              const yDocAssets: Y.Map<TDAsset> = yDoc.current.getMap('assets')
+              for (let asset of app.assets) {
+                const doc = yDoc.current.get(asset.id)
+                if (doc) {
+                  if (
+                    // TODO improve logic to filter out actions which are irrelevant.
+                    JSON.stringify(doc) !== JSON.stringify(asset)
+                  ) {
+                    yDocAssets.set(asset.id, asset)
+                  }
+                } else {
+                  yDocAssets.set(asset.id, asset)
                 }
               }
             })
-            // delete everything that previously existed but no longer exists.
-            if (previousStateObjectIds.current) {
-              for (let id of previousStateObjectIds.current.keys()) {
-                yDocShapes.delete(id)
-              }
-            }
-            previousStateObjectIds.current = newStateObjectIds
-          })
-
-          /********* ASSETS *********/
-          yDoc.transact(() => {
-            const yDocAssets: Y.Map<TDAsset> = yDoc.getMap('assets')
-            for (let asset of app.assets) {
-              const doc = yDoc.get(asset.id)
-              if (doc) {
-                if (
-                  // TODO improve logic to filter out actions which are irrelevant.
-                  JSON.stringify(doc) !== JSON.stringify(asset)
-                ) {
-                  yDocAssets.set(asset.id, asset)
-                }
-              } else {
-                yDocAssets.set(asset.id, asset)
-              }
-            }
-          })
+          } catch (e) {
+            console.error(e)
+          }
         }}
         onChangePresence={(app: TldrawApp, user: TDUser) => {
           ownAwarenessUpdateSubject({
             ...user,
             color: cursorColor,
+            metadata: { timestamp: Date.now() },
           })
         }}
         onChange={(app, reason) => {
-          console.log(reason)
-          
+          if (reason && !reason.startsWith('session') && !reason.startsWith('room:self')) {
+            console.log('reason', reason)
+          }
         }}
       />
     </div>
   )
 })
 
-
-
-function useTlDrawUpdates() : [Uint8Array | undefined, (update: Uint8Array) => void] {
+function useTlDrawUpdates(): [Uint8Array | undefined, (update: Uint8Array) => void] {
   const [update, setUpdate] = React.useState<Uint8Array>()
-  const throttledValue = useThrottle(update, 200);
+  const throttledValue = useThrottle(update, 200)
   return [throttledValue, setUpdate]
 }
 
-function useTlDrawAwarenessUpdates() : [AwarenessItem | undefined, (update: AwarenessItem) => void] {
+function useTlDrawAwarenessUpdates(): [AwarenessItem | undefined, (update: AwarenessItem) => void] {
   const [update, setUpdate] = React.useState<AwarenessItem>()
-  const throttledValue = useThrottle(update, 100);
+  const throttledValue = useThrottle(update, 100)
   return [throttledValue, setUpdate]
 }
 
+function useTraceUpdate(props) {
+  const prev = React.useRef(props)
+  React.useEffect(() => {
+    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
+      if (prev.current[k] !== v) {
+        ps[k] = [prev.current[k], v]
+      }
+      return ps
+    }, {})
+    if (Object.keys(changedProps).length > 0) {
+      console.log('Changed props:', changedProps)
+    }
+    prev.current = props
+  })
+}
